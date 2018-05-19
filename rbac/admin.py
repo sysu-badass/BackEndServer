15f7b2ac337c704a9091ff4f85aa1ad108ee2750
ishoping <2859098211@qq.com>
2018-05-19 17:39:25 +0800

    add database and dao

--- rbac/admin.py
+++ rbac/admin.py
@@ -19,25 +19,7 @@ class Admin:
             result.append(permission.url)
         return result
 
-    # def addUser(self, username, password):
-    #     usr = User(username=username, password=password)
-    #     db.session.add(usr)
-    #     db.session.commit()
     
-    # def addRole(self, rolename):
-    #     role = Role(rolename=rolename)
-    #     db.session.add(role)
-    #     db.session.commit()
-    
-    # def addPermission(self, url):
-    #     permission = Permission(url=url)
-    #     db.session.add(permission)
-    #     db.session.commit()
-    
-    # def addPermToRole(self, url, rolename):
-    #     permission = Permission(url=url)
-    #     role = Role(rolename=rolename)
-    #     role
 
     
 
--- rbac/models.py
+++ rbac/models.py
@@ -3,31 +3,42 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 user_role = db.Table('user_role',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id'), primary_key=True)
+    db.Column('user_id', db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('role_id', db.Integer,
+        db.ForeignKey('role.id', ondelete='CASCADE'), primary_key=True)
 )
 
 role_permission = db.Table('role_permission',
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id'), primary_key=True),
-    db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'), primary_key=True)
+    db.Column('role_id', db.Integer,
+        db.ForeignKey('role.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('permission_id', db.Integer, 
+        db.ForeignKey('permission.id', ondelete='CASCADE'), primary_key=True)
 )
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(45), nullable=False)
+    username = db.Column(db.String(45), nullable=False, unique=True)
     password = db.Column(db.String(45), nullable=False)
+    phone_number = db.Column(db.String(45))
+    image = db.Column(db.String(45))
 
     roles = db.relationship('Role', secondary='user_role',
                             backref=db.backref('users'))
 
-
     def __repr__(self):
         return '<User %r>' % self.username
 
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rolename = db.Column(db.String(45), nullable=False, unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.rolename
 
 class Permission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(100), nullable=False, unique=True)
 
     roles = db.relationship('Role', secondary='role_permission',
                             backref=db.backref('permissions'))
@@ -35,11 +46,5 @@ class Permission(db.Model):
     def __repr__(self):
         return '<Permission %r>' % self.url
 
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    rolename = db.Column(db.String(45), nullable=False)
-
-    def __repr__(self):
-        return '<Role %r>' % self.rolename
 
 
