b5fcd057ed36402d532f081c8ebdd31c28a52673
Joey <413486687@qq.com>
2018-06-26 12:38:01 +0800

    modify ./test/test_flask_restful.py

--- python-flask学习笔记.md
+++ python-flask学习笔记.md
@@ -7,6 +7,7 @@
 + 首先若是在virtualenv中建立project，首先需要建立虚拟环境先，使用 virtualenv 命令在project文件夹中创建 Python 虚拟环境。这个命令只有一个必需的参数,即虚拟环境的名字。创建虚拟环境后,当前文件夹中会出现一个子文件夹,名字就是上述命令中指定的参数,与虚拟环境相关的文件都保存在这个子文件夹中。按照惯例,一般虚拟环境会被命名为venv,如**virtualenv venv**就可以创建虚拟环境了。 **(p26)**
 + Linux环境下可以通过**source venv/bin/activate**启动虚拟环境，并通过**deactivate**关闭虚拟环境。
 + 要运行Flask程序，需要以下命令，在Linux环境下，我们需要告诉系统Flask app是哪一个文件。先**export FLASK_APP = main.py**，再**flask run**。这里的main.py文件指的是我们的程序实例，里面有视图函数以及路由等信息。
++ 若想进入debug模式，需要 **app.run(debug=True)** 在app.run()函数里面添加参数debug，然后需要命令**export FLASK_DEBUG=1**，之后在运行命令 **flask run** 即可
 
 
 ### python
--- test/test_flask_restful.py
+++ test/test_flask_restful.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,7 +8,22 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+#在以下三个URI中都会返回HelloWorld class的资源
+api.add_resource(HelloWorld, '/', '/hello', '/world')
+
+#需要作为global的数据来存储
+todos = {}
+
+class TodoSimple(Resource):
+
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
