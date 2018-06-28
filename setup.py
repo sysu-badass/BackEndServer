0b4a50fe2cf511c18b32fd76dc11447c801e8404
ishoping <2859098211@qq.com>
2018-06-28 14:53:26 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from app import db
+from app.database.models import User
+from app.database.models import Role
+from app.database.models import Permission
+from app import create_app
+from config import config
+
+config_name = 'development'
+app = create_app(config[config_name])
+
+db.create_all()
+print(db)
+user = User(username='lisi', password='123')
+role = Role(rolename='teacher')
+permission = Permission(url='/edit')
+
+role.permissions.append(permission)
+user.roles.append(role)
+
+db.session.add(user)
+db.session.commit()
+
+
+
